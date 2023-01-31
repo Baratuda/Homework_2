9c90c7447622f9acd32890e2a8d6af204e6932f2
Egor Baranovskiy <egorpifagop@gmail.com>
2023-01-31 18:58:23 +0300

    First task was done.

---
+++ Desktop/Homework_2/0/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+int x=0;
+
+while(x==0){
+
+Console.Write("Please input number: ");
+int number = int.Parse(Console.ReadLine());
+if(number>99 && number<1000){
+   int secondNumber = (number/10)%10;
+   Console.Write("-------------------------------------------------------- \n");
+   Console.Write($"Second number of the number {number} is {secondNumber} \n");
+   Console.Write("-------------------------------------------------------- \n");
+   break;
+} else {
+   Console.Write("-------------------------------------------------------- \n");
+   Console.Write("Inputed number isn't three-digit, please try again \n");
+   Console.Write("-------------------------------------------------------- \n");
+   
+}
+}
+
